fc8645c530014d36798d2e008d4d680f85f0eb29
Web_Design <Web_Design@MSDN>
2017-08-19 16:24:07 +0900

    adsfasdf

--- jsp1/WebContent/cal.jsp
+++ jsp1/WebContent/cal.jsp
@@ -2,6 +2,11 @@
     pageEncoding="UTF-8"%>
 <%@ page import="java.util.List" %>
 <%@ page import="java.util.ArrayList" %>
+<%@ page import="java.sql.Connection" %>
+<%@ page import="java.sql.DriverManager" %>
+<%@ page import="java.sql.ResultSet" %>
+<%@ page import="java.sql.SQLException" %>
+<%@ page import="java.sql.Statement" %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
--- jsp1/src/Exam.java
+++ jsp1/src/Exam.java
@@ -1,10 +1,27 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Exam {
 
 	public static void main(String[] args) {
-		List<String> list = new ArrayList<String>();
-		System.out.println(list);
+		Map map = new HashMap();
+		map.put("key1", "값이다~");
+		map.put("key2", "값이다~");
+		map.put("key3", "값이다~");
+		map.put("key4", "값이다~");
+		map.put("key5", "값이다~");
+		map.put("key6", "값이다~");
+		map.put("key7", "값이다~");
+		map.put("key8", "값이다~");
+		map.put("key9", "값이다~");
+		
+		Iterator it = map.keySet().iterator();
+		while(it.hasNext()) {
+			String key = it.next().toString();
+			System.out.print("key =>" + key);
+			System.out.println(",value =>" + map.get(key));
+		}
+		
 	}
 }
