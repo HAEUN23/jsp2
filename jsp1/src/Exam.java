7b86be9852a7505a29b30eb6344d0f47b909c310
Web_Design <Web_Design@MSDN>
2017-08-19 11:47:12 +0900

    1

---
+++ jsp1/src/Exam.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exam {
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		System.out.println(list);
+	}
+}
