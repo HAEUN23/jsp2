f966c6b261ae70fe66a0dc8690b23b2605c43772
Web_Design <Web_Design@MSDN>
2017-09-03 12:29:05 +0900

    t

--- jsp2/src/sql/board.sql
+++ jsp2/src/sql/board.sql
@@ -12,10 +12,28 @@ select * from board;
 insert into board(title, content, reg_Date, writer)
 values('test5','냉무2',now(),8);
 
-select * from user;
+update user
+set name='김경훈'
+where user_no=8
+;
 
 delete from user
 where user_no=7;
 
-select * from user, board
-where user.user_no = board.writer;
+
+select b.*, u.name
+from user as u , board as b
+where u.user_no = b.writer;
+
+select * from board;
+
+select * from board2;
+
+update  board2 b2
+set b2.title=
+(select b1.title from board b1 where b2.b_num=b1.b_num)
+;
+
+update board
+set title ='test2'
+where b_num=2;
